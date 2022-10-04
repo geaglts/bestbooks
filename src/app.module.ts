0648c20d19ca0f8241a411aa07b96f3bb65fad48
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-10-03 21:03:52 -0500

    remove controllers and providers of BooksModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PublishersController } from './books/publishers/publishers.controller';
-import { CategoriesController } from './books/categories/categories.controller';
 import { UsersController } from './users/users.controller';
 import { ShoppingsController } from './users/shoppings/shoppings.controller';
 import { FavoritesController } from './users/favorites/favorites.controller';
 import { BooksController } from './books/books.controller';
-import { AuthorsController } from './books/authors/authors.controller';
-import { PublishersService } from './books/publishers/publishers.service';
-import { CategoriesService } from './books/categories/categories.service';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
 
@@ -17,14 +12,11 @@ import { UsersModule } from './users/users.module';
   imports: [BooksModule, UsersModule],
   controllers: [
     AppController,
-    PublishersController,
-    CategoriesController,
     UsersController,
     ShoppingsController,
     FavoritesController,
     BooksController,
-    AuthorsController,
   ],
-  providers: [AppService, PublishersService, CategoriesService],
+  providers: [AppService],
 })
 export class AppModule {}
